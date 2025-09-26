99057776493fca6dfac51b581f0bb15c26abf69f
iaroslavaristov <iaroslavaristov@gmail.com>
2025-09-26 10:05:20 +0700

    Task structure initialized

--- task-tracker/internal/task.go
+++ task-tracker/internal/task.go
@@ -1,1 +1,30 @@
 package internal
+
+import "time"
+
+type Status string
+
+const (
+	StatusToDo       Status = "todo"
+	StatusDone       Status = "done"
+	StatusInProgress        = "in-progress"
+	StatusNone              = "none"
+)
+
+type Task struct {
+	ID          int64     `json:"id"`
+	Description string    `json:"description"`
+	Status      Status    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func NewTask(id int64, description string, status Status) *Task {
+	return &Task{
+		ID:          id,
+		Description: description,
+		Status:      status,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
