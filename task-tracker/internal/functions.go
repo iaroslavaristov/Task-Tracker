07482c518e51aa4f6cb6cfc85827d84821739f88
iaroslavaristov <iaroslavaristov@gmail.com>
2025-09-26 10:21:30 +0700

    Function for getting filepath and reading tasks from file was successfully implemented

--- task-tracker/internal/functions.go
+++ task-tracker/internal/functions.go
@@ -1,1 +1,61 @@
 package internal
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+)
+
+func getFilePath() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Println("Cannot access current working directory")
+		return ""
+	}
+
+	return path.Join(cwd, "tasks.json")
+}
+
+func ReadTasksFromFile() ([]Task, error) {
+	filepath := getFilePath()
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		file, err := os.Create(filepath)
+		os.WriteFile(filepath, []byte("[]"), 0644)
+
+		if err != nil {
+			log.Println("Cannot create a file")
+			return nil, err
+		}
+
+		defer func() {
+			if err := file.Close(); err != nil {
+				log.Println("Cannot close the file")
+			}
+		}()
+
+		return []Task{}, nil
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Println("Cannot open the file")
+		return nil, err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println("Cannot close the file")
+		}
+	}()
+
+	tasks := []Task{}
+	err = json.NewDecoder(file).Decode(&tasks)
+	if err != nil {
+		log.Println("Cannot decode the file")
+		return nil, err
+	}
+
+	return tasks, nil
+}
